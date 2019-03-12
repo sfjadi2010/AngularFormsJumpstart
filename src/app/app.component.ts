e99965948099ef7c02604c30eb02a9b93534489d
Salman Farsi <sfjadi2010@users.noreply.github.com>
2019-03-11 20:09:40 -0400

    Initial Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularFormsJumpstart';
+  title = 'Angular Forms Jumpstart';
 }
